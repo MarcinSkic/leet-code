bf5b053c3a1aebc46d3714a2ff41f69970e9eb4d
Marcin Skic <marcinskicoficjal@gmail.com>
2023-11-16 03:00:17 +0100

    Do first leet code exercise

---
+++ src/ex0001_two_sum.rs
@@ -0,0 +1,17 @@
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut v: Vec<i32> = Vec::new();
+
+    for (first_index, first_num) in nums.iter().enumerate() {
+        for (second_index, second_num) in nums[first_index + 1..nums.len()].iter().enumerate() {
+            let second_index = second_index + first_index + 1;
+            if first_num + second_num == target {
+                v.push(first_index.try_into().unwrap());
+                v.push(second_index.try_into().unwrap());
+
+                return v;
+            }
+        }
+    }
+
+    v
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub mod ex0001_two_sum;
+
+fn main() {
+    println!("{:#?}", ex0001_two_sum::two_sum(vec![3, 2, 4], 6));
+}
