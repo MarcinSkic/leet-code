e19a29ed6a6c0879a09528149ba45716046169c1
Marcin Skic <marcinskicoficjal@gmail.com>
2023-11-20 01:40:15 +0100

    Add more time efficient solution

--- src/ex0001_two_sum.rs
+++ src/ex0001_two_sum.rs
@@ -1,4 +1,6 @@
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+use std::collections::HashMap;
+
+pub fn two_sum_memory_efficient(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut v: Vec<i32> = Vec::new();
 
     for (first_index, first_num) in nums.iter().enumerate() {
@@ -9,6 +11,29 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
                 return v;
             }
+        }
+    }
+
+    v
+}
+
+pub fn two_sum_time_efficient(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut v: Vec<i32> = Vec::new();
+    let mut h: HashMap<i32, usize> = HashMap::new();
+
+    for (index, num) in nums.iter().enumerate() {
+        h.insert(target - num, index);
+    }
+
+    for (index, num) in nums.iter().enumerate() {
+        if let Some(x) = h.get(num) {
+            if *x == index {
+                continue;
+            }
+            v.push(index.try_into().unwrap());
+            v.push((*x).try_into().unwrap());
+
+            return v;
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 pub mod ex0001_two_sum;
 
 fn main() {
-    println!("{:#?}", ex0001_two_sum::two_sum(vec![3, 2, 4], 6));
+    println!(
+        "{:#?}",
+        ex0001_two_sum::two_sum_memory_efficient(vec![3, 2, 4], 6)
+    );
 }
